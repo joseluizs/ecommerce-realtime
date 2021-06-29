fc825171064a0a0b10f971153d2b2d1cf76dea46
joseluizs <joseluiz.smarques@gmail.com>
2021-06-29 20:27:44 -0300

    Trails create

--- app/Models/User.js
+++ app/Models/User.js
@@ -20,6 +20,12 @@ class User extends Model {
       }
     })
   }
+
+  static get hidden(){
+    return ['password']
+  }
+
+
   static get traits () {
     return [
       '@provider:Adonis/Acl/HasRole',
@@ -39,6 +45,19 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  image(){
+    return this.belongsTo('App/Models/Image')
+  }
+
+  coupons(){
+    return this.belongsToMany('App/Models/Coupon')
+  }
+
+
+
+
+
 }
 
 module.exports = User

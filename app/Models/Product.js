12d8ff60873f86a3c7e4e18f12fcc20364cc7551
joseluizs <joseluiz.smarques@gmail.com>
2021-07-05 21:47:08 -0300

    Model Product

---
+++ app/Models/Product.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Product extends Model {
+
+    image(){
+        return this.belongsTo('App/Models/Image')
+    }
+
+    /**
+     * Relacionamento entre produto e imagens
+     * Galeria de imagens do produto
+     */
+
+     images(){
+        return this.belongsToMany('App/Models/Image')
+    }
+
+    /**
+     * 
+     * Relacionamento entre produtos e categorias
+     */
+    categories(){
+        return this.belongsToMany('App/Models/Category')
+    }
+
+    /**
+     * Relacionameto entre produtos e cupons de descontos
+     */
+    coupons(){
+        return this.belongsToMany('App/Models/Coupon')
+    }
+
+}
+
+module.exports = Product
